ac28586843b382db2b2eab8f08732488cb7e9159
Verkhovskiy Nikita <nekit.verh@bk.ru>
2025-02-23 18:59:34 +0700

    add notes fragment on 90%(don't write recycle)

--- app/src/main/java/com/example/mydiary/presentation/custom_elements/ProgressBarView.kt
+++ app/src/main/java/com/example/mydiary/presentation/custom_elements/ProgressBarView.kt
@@ -1,20 +1,27 @@
 package com.example.mydiary.presentation.custom_elements
 
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.LinearGradient
+import android.graphics.Matrix
 import android.graphics.Paint
+import android.graphics.RectF
 import android.graphics.Shader
+import android.graphics.SweepGradient
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.util.Log
 import android.util.TypedValue
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.core.content.ContextCompat
 import com.example.mydiary.R
+import kotlin.math.cos
+import kotlin.math.sin
 
 class ProgressBarView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null,
@@ -33,12 +40,7 @@ class ProgressBarView @JvmOverloads constructor(
     private var foregroundStrokeCap: Types = Types.SQUARE
     private var strokeWidth: Float = DEFAULT_STROKE_WIDTH
     private var progresses: MutableList<Float> = mutableListOf(PROGRESS_DEFAULT)
-    private var startAngel: Int = DEFAULT_START_ANGEL
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-
-    }
+    private var startAngel: Float = DEFAULT_START_ANGEL
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -52,15 +54,14 @@ class ProgressBarView @JvmOverloads constructor(
 
             setBackground(typedArray)
             setStroke(typedArray)
-            Log.d("progress_bar_view", "finish4")
             setAutoCalculate(typedArray)
             setThickness(typedArray)
             setForegroundStrokeCap(typedArray)
             setStartAngle(typedArray)
             setProgresses(typedArray)
             setStrokeWidth(typedArray)
-
             typedArray.recycle()
+            startAnimation()
 
         }
     }
@@ -101,8 +102,7 @@ class ProgressBarView @JvmOverloads constructor(
                         try {
                             ringStrokeColors.add(
                                 Pair(
-                                    ContextCompat.getColor(context, itemResId),
-                                    index
+                                    ContextCompat.getColor(context, itemResId), index
                                 )
                             )
                         } catch (e: Exception) {
@@ -119,8 +119,7 @@ class ProgressBarView @JvmOverloads constructor(
                             }
                         } catch (e: Exception) {
                             Log.d(
-                                "progress_bar_view",
-                                "Элемент с индексом $index не является цветом"
+                                "progress_bar_view", "Элемент с индексом $index не является цветом"
                             )
                         }
                     }
@@ -174,26 +173,35 @@ class ProgressBarView @JvmOverloads constructor(
         if (resId != -1) {
             val ta = resources.obtainTypedArray(resId)
             sizeArraysMin = ta.length().coerceAtMost(sizeArraysMin)
-            progresses = List(ta.length()) { index ->
-                ta.getFloat(index, 0f) as? Float
-                    ?: throw IllegalArgumentException("Float at index $index is not a Float")
-            }.toMutableList()
-            if ((progresses.sum() > 1f || progresses.sum() < 0f) && autoCalculatePart)
-                throw IllegalArgumentException("invalid array float, not in [0,1]")
-        }
-
-        if (autoCalculatePart) {
-            val sum = progresses.sum()
-            for (part in progresses.indices) {
-                progresses[part] = progresses[part] / sum
+            for (index in 0 until ta.length()) {
+                val tv = ta.peekValue(index)
+                if (tv != null) {
+                    Log.d("progress_bar_view", "tv.type: ${tv.type}")
+                    Log.d("progress_bar_view", "index: $index")
+                    if (tv.type != TypedValue.TYPE_FLOAT) throw IllegalArgumentException("invalid array float")
+                    progresses.add(ta.getFloat(index, 0f))
+                }
             }
+
+            if ((progresses.sum() > 1f || progresses.sum() < 0f) && autoCalculatePart) throw IllegalArgumentException(
+                "invalid array float, not in [0,1]"
+            )
+
+            if (autoCalculatePart) {
+                val sum = progresses.sum()
+                for (part in progresses.indices) {
+                    progresses[part] = progresses[part] / sum
+                }
+            }
+        } else {
+            sizeArraysMin = 1
         }
 
 
     }
 
     private fun setStartAngle(typedArray: TypedArray) {
-        startAngel = typedArray.getInt(R.styleable.ProgressBarView_startAngle, -1) % 360
+        startAngel = typedArray.getFloat(R.styleable.ProgressBarView_startAngle, 0f) % 360
     }
 
     private fun setStrokeWidth(typedArray: TypedArray) {
@@ -215,10 +223,8 @@ class ProgressBarView @JvmOverloads constructor(
 
         val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             style = Paint.Style.STROKE
-            if (ringBackgroundGradient == null)
-                color = ringBackgroundColor
-            else
-                ringBackgroundGradient?.let { shader = convertGradientToShader(it) }
+            if (ringBackgroundGradient == null) color = ringBackgroundColor
+            else ringBackgroundGradient?.let { shader = convertGradientToShader(it) }
         }
 
 
@@ -226,6 +232,21 @@ class ProgressBarView @JvmOverloads constructor(
         val innerRadius = center - thickness / 2f
 
         canvas.drawCircle(cx, cy, innerRadius, backgroundPaint)
+    }
+
+
+    private fun startAnimation() {
+        ValueAnimator.ofFloat(0f, 360f).apply {
+            duration = 10000L
+            repeatMode = ValueAnimator.RESTART
+            interpolator = LinearInterpolator()
+            repeatCount = ValueAnimator.INFINITE
+            addUpdateListener {
+                startAngel = it.animatedValue as Float
+                invalidate()
+            }
+            start()
+        }
     }
 
     private fun createStrokeProgressBar(canvas: Canvas) {
@@ -239,32 +260,83 @@ class ProgressBarView @JvmOverloads constructor(
         val thickness =
             if (ringThickness.second) ringThickness.first else center * ringThickness.first
 
-        var iColor = 0
-        var iGradient = 0
-        for (pos in 0 until sizeArraysMin) {
-            if (iColor < ringStrokeColors.size && ringStrokeColors[iColor].second == pos) {
-                val value = ringStrokeColors[iColor]
-                iColor++
-            }
-            if (iGradient < ringStrokeGradients.size && ringStrokeGradients[iGradient].second == pos) {
-                val value = ringStrokeGradients[iGradient]
-                iGradient++
-            }
-        }
-
-        val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        val strokePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             style = Paint.Style.STROKE
-            if (ringBackgroundGradient == null)
-                color = ringBackgroundColor
-            else
-                ringBackgroundGradient?.let { shader = convertGradientToShader(it) }
+            strokeWidth = thickness
+
         }
-
-
-        backgroundPaint.strokeWidth = thickness
         val innerRadius = center - thickness / 2f
 
-        canvas.drawCircle(cx, cy, innerRadius, backgroundPaint)
+
+        val rect = RectF(
+            cx - innerRadius, cy - innerRadius, cx + innerRadius, cy + innerRadius
+        )
+        val sweepAngle = 90f
+
+        val startColor = ContextCompat.getColor(context, R.color.logbook_gradient_yellow_1)
+        val endColor = ContextCompat.getColor(context, R.color.logbook_gradient_yellow_2)
+        val shader =
+            SweepGradient(cx, cy, intArrayOf(startColor, endColor), floatArrayOf(0f, 0.25f))
+
+        val matrix = Matrix()
+        matrix.preRotate(startAngel - 90f, cx, cy)
+        shader.setLocalMatrix(matrix)
+
+        /*when(foregroundStrokeCap){
+            Types.BUTT->{ strokePaint.strokeCap = Paint.Cap.BUTT}
+            Types.ROUND->{ strokePaint.strokeCap = Paint.Cap.ROUND}
+            Types.SQUARE->{ strokePaint.strokeCap = Paint.Cap.SQUARE}
+        }*/
+        strokePaint.shader = shader
+
+
+        canvas.drawArc(rect, startAngel - 90f, sweepAngle, false, strokePaint)
+        val startAngleRad = Math.toRadians((startAngel - 90f).toDouble())
+        val endAngleRad = Math.toRadians((startAngel - 90f + sweepAngle).toDouble())
+
+        val startX = cx + innerRadius * cos(startAngleRad).toFloat()
+        val startY = cy + innerRadius * sin(startAngleRad).toFloat()
+        val endX = cx + innerRadius * cos(endAngleRad).toFloat()
+        val endY = cy + innerRadius * sin(endAngleRad).toFloat()
+
+        val endpointPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.FILL
+        }
+
+        endpointPaint.color = startColor
+        canvas.drawCircle(startX, startY, thickness / 2f, endpointPaint)
+
+        endpointPaint.color = endColor
+        canvas.drawCircle(endX, endY, thickness / 2f, endpointPaint)
+
+
+        /*        var iColor = 0
+                var iGradient = 0
+                Log.d("progress_bar_view", "progresses: $progresses")
+
+                for (pos in 0 until sizeArraysMin) {
+                    if (iColor < ringStrokeColors.size && ringStrokeColors[iColor].second == pos) {
+                        val color = ringStrokeColors[iColor]
+                        strokePaint.color = color.first
+                        strokePaint.shader = null
+                        canvas.drawArc(rect, startAngleLocal, progresses[pos] * 360, false, strokePaint)
+                        startAngleLocal += progresses[pos] * 360
+                        iColor++
+                    }
+                    if (iGradient < ringStrokeGradients.size && ringStrokeGradients[iGradient].second == pos) {
+                        Log.d("progress_bar_view", "pos: $pos")
+                        val gradient = ringStrokeGradients[iGradient]
+                        strokePaint.color = Color.RED
+                        Log.d("progress_bar_view", "progresses: ${progresses[pos]}")
+                        strokePaint.shader = convertGradientToShader(gradient.first) convertGradientToShader(ContextCompat.getDrawable(context,R.drawable.logbook_gradient_red) as GradientDrawable)
+
+                        canvas.drawArc(rect, startAngleLocal, progresses[pos] * 360, false, strokePaint)
+                        startAngleLocal += progresses[pos] * 360
+                        iGradient++
+                    }
+                    Log.d("progress_bar_view", "startAngleLocal: $startAngleLocal")
+
+                }*/
     }
 
     private fun convertGradientToShader(gradientDrawable: GradientDrawable): LinearGradient {
@@ -273,31 +345,19 @@ class ProgressBarView @JvmOverloads constructor(
             GradientDrawable.Orientation.LEFT_RIGHT -> floatArrayOf(0f, 0f, width.toFloat(), 0f)
             GradientDrawable.Orientation.TOP_BOTTOM -> floatArrayOf(0f, 0f, 0f, height.toFloat())
             GradientDrawable.Orientation.BL_TR -> floatArrayOf(
-                width.toFloat(),
-                0f,
-                0f,
-                height.toFloat()
+                width.toFloat(), 0f, 0f, height.toFloat()
             )
 
             GradientDrawable.Orientation.BR_TL -> floatArrayOf(
-                0f,
-                height.toFloat(),
-                width.toFloat(),
-                0f
+                0f, height.toFloat(), width.toFloat(), 0f
             )
 
             GradientDrawable.Orientation.TL_BR -> floatArrayOf(
-                0f,
-                0f,
-                width.toFloat(),
-                height.toFloat()
+                0f, 0f, width.toFloat(), height.toFloat()
             )
 
             GradientDrawable.Orientation.TR_BL -> floatArrayOf(
-                width.toFloat(),
-                0f,
-                0f,
-                height.toFloat()
+                width.toFloat(), 0f, 0f, height.toFloat()
             )
 
             else -> floatArrayOf(0f, 0f, width.toFloat(), 0f)
@@ -317,10 +377,10 @@ class ProgressBarView @JvmOverloads constructor(
         const val PROGRESS_DEFAULT = 0.5f
 
         enum class Types {
-            BUTT, TRIANGULAR, SQUARE
+            BUTT, ROUND, SQUARE
         }
 
-        const val DEFAULT_START_ANGEL = 0
+        const val DEFAULT_START_ANGEL = 0f
         const val RING_THICKNESS_DEFAULT = 4f
     }
 }
--- app/src/main/java/com/example/mydiary/presentation/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/example/mydiary/presentation/fragments/StatisticsFragment.kt
@@ -1,6 +1,18 @@
 package com.example.mydiary.presentation.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.mydiary.R
 
 class StatisticsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.notes_fragment,container,false)
+    }
 }
