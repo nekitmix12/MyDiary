efd286d78ae24d6e6edaf53c51330a47b2a74307
Verkhovskiy Nikita <nekit.verh@bk.ru>
2025-02-23 23:02:22 +0700

    add settings fragment on 60%(don't write recycle and timepicker)

--- app/src/main/java/com/example/mydiary/presentation/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/mydiary/presentation/fragments/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mydiary.presentation.fragments
 
 import androidx.fragment.app.Fragment
+import com.example.mydiary.R
 
-class SettingsFragment: Fragment(){
+class SettingsFragment: Fragment(R.layout.settings_param){
 }
